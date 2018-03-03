28026aa80a658d1f15b1b7d019a38d5894bf2564
demons <837940593@qq.com>
2018-03-03 17:01:28 +0800

    .

---
+++ Spring/SpringSecurity/loveu/src/main/java/com/viviansj/loveu/LoveuApplication.java
@@ -0,0 +1,12 @@
+package com.viviansj.loveu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveuApplication.class, args);
+	}
+}
---
+++ Spring/SpringSecurity/loveu/src/main/java/com/viviansj/loveu/LoveuController.java
@@ -0,0 +1,15 @@
+package com.viviansj.loveu;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoveuController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String sayLoveu() {
+        return "Loveu!";
+    }
+}
