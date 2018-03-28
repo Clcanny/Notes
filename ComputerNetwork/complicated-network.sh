8750a3475aa538ef7e94ce49b7887618a1642130
demons <837940593@qq.com>
2018-03-28 15:00:35 +0800

    make complicated-network.sh better

--- ComputerNetwork/complicated-network.sh
+++ ComputerNetwork/complicated-network.sh
@@ -16,6 +16,8 @@ docker exec router1 bash -c "ip route add 172.0.1.0/28 via 172.0.1.1 dev eth1"
 docker exec router1 bash -c "ip route add 172.0.2.0/28 via 172.0.3.2 dev eth2"
 # 显示新的路由规则
 docker exec router1 bash -c "ip route"
+# 查看是否允许转发
+docker exec router1 bash -c "cat /proc/sys/net/ipv4/ip_forward"
 
 docker run -itd --privileged --name router2 network:ubuntu
 docker network connect VMNet2 router2 --ip 172.0.2.1
@@ -31,6 +33,8 @@ docker exec router2 bash -c "ip route add 172.0.1.0/28 via 172.0.3.1 dev eth2"
 docker exec router2 bash -c "ip route add 172.0.2.0/28 via 172.0.2.1 dev eth1"
 # 显示新的路由规则
 docker exec router2 bash -c "ip route"
+# 查看是否允许转发
+docker exec router2 bash -c "cat /proc/sys/net/ipv4/ip_forward"
 
 docker run -itd --privileged --name container1.2 network:ubuntu
 docker network connect VMNet1 container1.2 --ip 172.0.1.2
@@ -56,21 +60,21 @@ docker network disconnect bridge container2.3
 # 设置gateway
 docker exec container2.3 bash -c "ip route change default via 172.0.2.1 dev eth1"
 
-docker exec container1.2 bash -c "ping 172.0.1.3 -c 4"
-docker exec container1.2 bash -c "ping 172.0.2.2 -c 4"
-docker exec container1.2 bash -c "ping 172.0.2.3 -c 4"
+docker exec container1.2 bash -c "ping 172.0.1.3 -c 4" | tail -1
+docker exec container1.2 bash -c "ping 172.0.2.2 -c 4" | tail -1
+docker exec container1.2 bash -c "ping 172.0.2.3 -c 4" | tail -1
 
-docker exec container1.3 bash -c "ping 172.0.1.2 -c 4"
-docker exec container1.3 bash -c "ping 172.0.2.2 -c 4"
-docker exec container1.3 bash -c "ping 172.0.2.3 -c 4"
+docker exec container1.3 bash -c "ping 172.0.1.2 -c 4" | tail -1
+docker exec container1.3 bash -c "ping 172.0.2.2 -c 4" | tail -1
+docker exec container1.3 bash -c "ping 172.0.2.3 -c 4" | tail -1
 
-docker exec container2.2 bash -c "ping 172.0.1.2 -c 4"
-docker exec container2.2 bash -c "ping 172.0.1.3 -c 4"
-docker exec container2.2 bash -c "ping 172.0.2.3 -c 4"
+docker exec container2.2 bash -c "ping 172.0.1.2 -c 4" | tail -1
+docker exec container2.2 bash -c "ping 172.0.1.3 -c 4" | tail -1
+docker exec container2.2 bash -c "ping 172.0.2.3 -c 4" | tail -1
 
-docker exec container2.3 bash -c "ping 172.0.1.2 -c 4"
-docker exec container2.3 bash -c "ping 172.0.1.3 -c 4"
-docker exec container2.3 bash -c "ping 172.0.2.2 -c 4"
+docker exec container2.3 bash -c "ping 172.0.1.2 -c 4" | tail -1
+docker exec container2.3 bash -c "ping 172.0.1.3 -c 4" | tail -1
+docker exec container2.3 bash -c "ping 172.0.2.2 -c 4" | tail -1
 
 # 清理工作
 docker rm -f router1 router2 container1.2 container1.3 container2.2 container2.3
