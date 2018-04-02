7531e00d653d7f2d0890639e319a0516b9281556
demons <837940593@qq.com>
2018-04-02 14:30:12 +0800

    reindent ping.h

--- ComputerNetwork/ping/ping/ping.cpp
+++ ComputerNetwork/ping/ping/ping.cpp
@@ -1,6 +1,7 @@
 #include "ping.h"
 
-Ping::Ping() {
+Ping::Ping()
+{
     m_maxPacketSize = 4;
     m_datalen = 56;
     m_nsend = 0;
@@ -16,20 +17,18 @@ unsigned short Ping::getChksum(unsigned short *addr,int len)
     unsigned short *w = addr;
     unsigned short answer = 0;
 
-    /*把ICMP报头二进制数据以2字节为单位累加起来*/
     while (nleft > 1)
     {
         sum += *w++;
         nleft -= 2;
     }
-    /*若ICMP报头为奇数个字节，会剩下最后一字节。把最后一个字节视为一个2字节数据的高字节，这个2字节数据的低字节为0，继续累加*/
     if (nleft == 1)
     {
         *(unsigned char *)(&answer) = *(unsigned char *)w;
         sum += answer;
     }
     sum = (sum >> 16) + (sum & 0xffff);
-    sum += (sum>>16);
+    sum += (sum >> 16);
     answer = ~sum;
     return answer;
 }
