a0d2e09893fd0d897238e414d03068eea9a99cac
demons <837940593@qq.com>
2018-04-02 14:35:41 +0800

    reindent main.cpp

--- ComputerNetwork/ping/ping/main.cpp
+++ ComputerNetwork/ping/ping/main.cpp
@@ -4,21 +4,26 @@
 void showPingResult(PingResult pingResult);
 int main(int argc,char *argv[])
 {
-    if (argc != 2) {
+    if (argc != 2)
+    {
         printf("usage: ping <host or ip>\n");
         return 0;
     }
+
     char* hostOrIp = argv[1];
     int nsend = 0, nreceived = 0;
     bool ret;
     PingResult pingResult;
     Ping ping = Ping();
-    for (int count = 1; count <= 4; count++) {
+    for (int count = 1; count <= 4; count++)
+    {
         ret = ping.ping(hostOrIp, 1, pingResult);
-        if (count == 1) {
+        if (count == 1)
+        {
             printf("PING %s(%s): %d bytes data in ICMP packets.\n", hostOrIp, pingResult.ip.c_str(), pingResult.dataLen);
         }
-        if (!ret) {
+        if (!ret)
+        {
             printf("%s\n", pingResult.error.c_str());
             break;
         }
@@ -26,24 +31,29 @@ int main(int argc,char *argv[])
         nsend += pingResult.nsend;
         nreceived += pingResult.nreceived;
     }
-    if (ret) {
-        printf("%d packets transmitted, %d received , %%%d lost\n", nsend, nreceived,
-            (nsend - nreceived) / nsend * 100);
+    if (ret)
+    {
+        printf("%d packets transmitted, %d received , %%%d lost\n", nsend, nreceived, (nsend - nreceived) / nsend * 100);
     }
     return 0;
 }
 
-void showPingResult(PingResult pingResult) {
-    for (unsigned int icmpEchoReplyIndex = 0; icmpEchoReplyIndex < pingResult.icmpEchoReplys.size(); icmpEchoReplyIndex++) {
+void showPingResult(PingResult pingResult)
+{
+    for (unsigned int icmpEchoReplyIndex = 0; icmpEchoReplyIndex < pingResult.icmpEchoReplys.size(); icmpEchoReplyIndex++)
+    {
         IcmpEchoReply icmpEchoReply = pingResult.icmpEchoReplys.at(icmpEchoReplyIndex);
-        if (icmpEchoReply.isReply) {
+        if (icmpEchoReply.isReply)
+        {
             printf("%d byte from %s: icmp_seq=%u ttl=%d rtt=%.3f ms\n",
                     icmpEchoReply.icmpLen,
                     icmpEchoReply.fromAddr.c_str(),
                     icmpEchoReply.icmpSeq,
                     icmpEchoReply.ipTtl,
                     icmpEchoReply.rtt);
-        } else {
+        }
+        else
+        {
             printf("request timeout\n");
         }
     }
