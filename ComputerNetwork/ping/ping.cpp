40caa01a5a7ca1c7d9fdce5fbacc0324e66c6442
demons <837940593@qq.com>
2018-04-02 14:42:04 +0800

    rearrange files in ComputerNetwork

--- ComputerNetwork/ping/ping.cpp
+++ ComputerNetwork/ping/ping.cpp
@@ -1,148 +1,270 @@
-#include <time.h>
-#include <unistd.h>
-#include <cassert>
-#include <stdio.h>
+#include "ping.h"
 
-#include <fcntl.h>
-#include <resolv.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/ip_icmp.h>
-
-class ICMP
+Ping::Ping()
 {
-    private:
-        static uint16_t sequenceIndex;
+    m_maxPacketSize = 4;
+    m_datalen = 56;
+    m_nsend = 0;
+    m_nreceived = 0;
+    m_icmp_seq = 0;
+}
 
-    public:
-        uint8_t type;
-        uint8_t code;
-        uint16_t checksum;
-        uint16_t identifier;
-        uint16_t sequenceNumber;
-        uint64_t timestamp;
-        char payload[8];
+/*校验和算法*/
+unsigned short Ping::getChksum(unsigned short *addr,int len)
+{   
+    int nleft = len;
+    int sum = 0;
+    unsigned short *w = addr;
+    unsigned short answer = 0;
 
-    private:
-        void makeChecksum()
-        {
-            uint16_t* buf = reinterpret_cast<uint16_t*>(this);
-            int len = sizeof(ICMP);
-            unsigned int sum = 0;
+    while (nleft > 1)
+    {
+        sum += *w++;
+        nleft -= 2;
+    }
+    if (nleft == 1)
+    {
+        *(unsigned char *)(&answer) = *(unsigned char *)w;
+        sum += answer;
+    }
+    sum = (sum >> 16) + (sum & 0xffff);
+    sum += (sum >> 16);
+    answer = ~sum;
+    return answer;
+}
 
-            for (; len > 1; len -= 2)
-            {
-                sum += (*buf)++;
-            }
-            if (len == 1)
-            {
-                sum += *reinterpret_cast<uint8_t*>(buf);
-            }
+/* 设置ICMP报头 */
+int Ping::packIcmp(int pack_no, struct icmp* icmp)
+{   
+    int i, packsize;
+    struct icmp *picmp;
+    struct timeval *tval;
 
-            sum = (sum >> 16) + (sum & 0xFFFF);
-            sum += (sum >> 16);
+    picmp = icmp;
+    picmp->icmp_type = ICMP_ECHO;
+    picmp->icmp_code = 0;
+    picmp->icmp_cksum = 0;
+    picmp->icmp_seq = pack_no;
+    picmp->icmp_id = m_pid;
+    packsize = 8 + m_datalen;
+    tval = (struct timeval *)icmp->icmp_data;
+    /* 记录发送时间 */
+    gettimeofday(tval, NULL);
+    picmp->icmp_cksum = getChksum((unsigned short *)icmp, packsize);
+    return packsize;
+}
 
-            checksum = ~sum;
-        }
+/* 剥去ICMP报头 */
+bool Ping::unpackIcmp(char *buf,int len, struct IcmpEchoReply *icmpEchoReply)
+{   
+    int i,iphdrlen;
+    struct ip *ip;
+    struct icmp *icmp;
+    struct timeval *tvsend, tvrecv, tvresult;
+    double rtt;
 
-        void display(void *buf, int bytes)
-        {
-            /* struct iphdr *ip = (struct iphdr*)buf; */
-            /* struct icmphdr *icmp = buf + ip->ihl * 4; */
-            
-            /* printf("----------------\n"); */
-            /* for (int i = 0; i < bytes; i++) */
-            /* { */
-            /* 	if (!(i & 15)) printf("\nX:  %d", i); */
-            /* 	printf("X %c", ((unsigned char*)buf)[i]); */
-            /* } */
-            /* printf("\n"); */
-            /* printf("IPv%d: hdr-size=%d pkt-size=%d protocol=%d TTL=%d src=%s ", */
-            /* 	ip->version, ip->ihl*4, ntohs(ip->tot_len), ip->protocol, */
-            /* 	ip->ttl, inet_ntoa(ip->saddr)); */
-            /* printf("dst=%s\n", inet_ntoa(ip->daddr)); */
-            /* if ( icmp->un.echo.id == pid ) */
-            /* { */
-            /* 	printf("ICMP: type[%d/%d] checksum[%d] id[%d] seq[%d]\n", */
-            /* 		icmp->type, icmp->code, ntohs(icmp->checksum), */
-            /* 		icmp->un.echo.id, icmp->un.echo.sequence); */
-            /* } */
-        }
+    ip = (struct ip *)buf;
+    /* 求ip报头长度,即ip报头的长度标志乘4 */
+    iphdrlen = ip->ip_hl << 2;
+    /* 越过IP报头，指向ICMP报头 */
+    icmp = (struct icmp *)(buf + iphdrlen);
+    /* ICMP报头及ICMP数据报的总长度 */
+    len -= iphdrlen;
+    /* 小于ICMP报头长度则不合理 */
+    if (len < 8)
+    {
+        printf("ICMP packets\'s length is less than 8\n");
+        return false;
+    }
+    /* 确保所接收的是我所发的的ICMP的回应 */
+    if ((icmp->icmp_type == ICMP_ECHOREPLY) && (icmp->icmp_id == m_pid))
+    {
+        tvsend = (struct timeval *)icmp->icmp_data;
+        /* 记录接受时间 */
+        gettimeofday(&tvrecv, NULL);
+        /* 接受和发送的时间差 */
+        tvresult = tvSub(tvrecv, *tvsend);
+        /* 以毫秒为单位计算RTT */
+        rtt=tvresult.tv_sec*1000 + tvresult.tv_usec/1000;
+        icmpEchoReply->rtt = rtt;
+        icmpEchoReply->icmpSeq = icmp->icmp_seq;
+        icmpEchoReply->ipTtl = ip->ip_ttl;
+        icmpEchoReply->icmpLen = len;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
-        void makeRequestHeader()
-        {
-            type = 8;
-            code = 0;
-            checksum = 0;
-
-            pid_t processId = getpid();
-            identifier = *reinterpret_cast<uint16_t*>(&processId);
-
-            sequenceNumber = sequenceIndex;
-            sequenceIndex++;
-
-            time_t t;
-            time(&t);
-            timestamp = *reinterpret_cast<uint64_t*>(&t);
-
-            for (int i = 0; i < 8; i++)
-            {
-                payload[i] = i + '\0';
-            }
-
-            makeChecksum();
-        }
-
-    public:
-        void request(struct sockaddr_in* addr)
-        {
-            makeRequestHeader();
-
-            const int val = 255;
-
-            /* int socket(int domain, int type, int protocol); */
-            /* int sd = socket(PF_INET, SOCK_RAW, proto->p_proto); */
-            int sd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
-            assert (sd >= 0);
-            /* getsockopt() and setsockopt() manipulate options for the socket referred to by the file descriptor sockfd.
-             * Options may exist at multiple protocol levels; they are always present at the uppermost socket level.
-             */
-            assert (setsockopt(sd, SOL_IP, IP_TTL, &val, sizeof(val)) == 0);
-            /* assert (fcntl(sd, F_SETFL, O_NONBLOCK) == 0); */
-
-            assert (sendto(sd, this, sizeof(ICMP), 0, (struct sockaddr*)addr, sizeof(struct sockaddr_in)) > 0);
-        }
-
-        void response()
-        {
-            unsigned char buf[1024];
-
-            /* int sd = socket(PF_INET, SOCK_RAW, proto->p_proto); */
-            int sd = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
-            assert (sd >= 0);
-
-            struct sockaddr_in addr;
-            int len = sizeof(addr);
-            int bytes = recvfrom(sd, buf, sizeof(buf), 0, (struct sockaddr*)&addr, (socklen_t*)&len);
-            assert (bytes > 0);
-            display(buf, bytes);
-        }
-};
-
-uint16_t ICMP::sequenceIndex = 1;
-
-int main()
+/* 两个timeval结构相减 */
+struct timeval Ping::tvSub(struct timeval timeval1, struct timeval timeval2)
 {
-    assert (sizeof(ICMP) == 1 + 1 + 2 + 2 + 2 + 8 + 8);
+    struct timeval result;
+    result = timeval1;
+    if (result.tv_usec < timeval2.tv_usec < 0)
+    {
+        --result.tv_sec;
+        result.tv_usec += 1000000;
+    }
+    result.tv_sec -= timeval2.tv_sec;
+    return result;
+}
 
-    struct hostent *hname = gethostbyname("127.0.0.1");
-    struct sockaddr_in addr;
-    /* bzero(&addr, sizeof(addr)); */
-    addr.sin_family = hname->h_addrtype;
-    addr.sin_port = 0;
-    addr.sin_addr.s_addr = *(long*)hname->h_addr;
+/* 发送三个ICMP报文 */
+bool Ping::sendPacket()
+{   
+    size_t packetsize;
+    while (m_nsend < m_maxPacketSize)
+    {
+        m_nsend++;
+        m_icmp_seq++;
+        /* 设置ICMP报头 */
+        packetsize = packIcmp(m_icmp_seq, (struct icmp*)m_sendpacket);
 
-    ICMP icmp;
-    icmp.request(&addr);
-    icmp.response();
+        if (sendto(m_sockfd, m_sendpacket, packetsize, 0, (struct sockaddr*)&m_dest_addr, sizeof(m_dest_addr)) < 0)
+        {
+            perror("sendto error");
+            continue;
+        }
+    }
+    return true;
+}
+
+/* 接收所有ICMP报文 */
+bool Ping::recvPacket(PingResult &pingResult)
+{
+    int len;
+    extern int errno;
+    struct IcmpEchoReply icmpEchoReply;
+    int maxfds = m_sockfd + 1;
+    int nfd = 0;
+    fd_set rset;
+    FD_ZERO(&rset);
+    socklen_t fromlen = sizeof(m_from_addr);
+    struct timeval timeout;
+    timeout.tv_sec = 4;
+    timeout.tv_usec = 0;
+
+    for (int recvCount = 0; recvCount < m_maxPacketSize; recvCount++)
+    {
+        FD_SET(m_sockfd, &rset);
+        if ((nfd = select(maxfds, &rset, NULL, NULL, &timeout)) == -1)
+        {
+            perror("select error");
+            continue;
+        }
+        if (nfd == 0)
+        {
+            /* recv time out */
+            icmpEchoReply.isReply = false;
+            pingResult.icmpEchoReplys.push_back(icmpEchoReply);
+            continue;
+        }
+        if (FD_ISSET(m_sockfd, &rset))
+        {
+            if ((len = recvfrom(m_sockfd, m_recvpacket, sizeof(m_recvpacket), 0, (struct sockaddr *)&m_from_addr, &fromlen)) < 0)
+            {
+                if(errno == EINTR)
+                {
+                    continue;
+                }
+                perror("recvfrom error");
+                continue;
+            }
+            icmpEchoReply.fromAddr = inet_ntoa(m_from_addr.sin_addr);
+            if (icmpEchoReply.fromAddr != pingResult.ip) {
+                /* retry again */
+                recvCount--;
+                continue;
+            }
+        }
+        if (unpackIcmp(m_recvpacket, len, &icmpEchoReply)==-1)
+        {
+            /* retry again */
+            recvCount--;
+            continue;
+        }
+        icmpEchoReply.isReply = true;
+        pingResult.icmpEchoReplys.push_back(icmpEchoReply);
+        m_nreceived++;
+    }
+    return true;
+}
+
+bool Ping::getsockaddr(const char * hostOrIp, struct sockaddr_in* sockaddr)
+{
+    struct hostent *host;
+    struct sockaddr_in dest_addr;
+    unsigned long inaddr = 0l;
+    bzero(&dest_addr, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    /* 判断是主机名还是ip地址 */
+    if (inaddr = inet_addr(hostOrIp) == INADDR_NONE)
+    {
+        /* 主机名 */
+        if ((host = gethostbyname(hostOrIp)) == NULL)
+        {
+            return false;
+        }
+        memcpy((char *)&dest_addr.sin_addr, host->h_addr, host->h_length);
+    }
+    /* IP地址 */
+    else if (!inet_aton(hostOrIp, &dest_addr.sin_addr))
+    {
+        return false;
+    }
+    *sockaddr = dest_addr;
+    return true;
+}
+
+bool Ping::ping(std::string host, PingResult& pingResult)
+{
+    return ping(host, 1, pingResult);
+}
+
+bool Ping::ping(std::string host, int count, PingResult& pingResult)
+{
+    int size = 50 * 1024;
+    IcmpEchoReply icmpEchoReply;
+    bool ret;
+
+    m_nsend = 0;
+    m_nreceived = 0;
+    pingResult.icmpEchoReplys.clear();
+    m_maxPacketSize = count;
+    m_pid = getpid();
+
+    pingResult.dataLen = m_datalen;
+
+    /* 生成使用ICMP的原始套接字,这种套接字只有root才能生成 */
+    if ((m_sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0)
+    {
+        extern int errno;
+        pingResult.error = strerror(errno);
+        return false;
+    }
+    /* 扩大套接字接收缓冲区到50K
+     * 这样做主要为了减小接收缓冲区溢出的的可能性
+     * 若无意中ping一个广播地址或多播地址
+     * 将会引来大量应答
+     */
+    setsockopt(m_sockfd, SOL_SOCKET, SO_RCVBUF, &size, sizeof(size));
+
+    /* 获取main的进程id,用于设置ICMP的标志符 */
+    if (!getsockaddr(host.c_str(), &m_dest_addr))
+    {
+        pingResult.error = "unknow host " + host;
+        return false;
+    }
+    pingResult.ip = inet_ntoa(m_dest_addr.sin_addr);
+    /* 发送所有ICMP报文 */
+    sendPacket();
+    /* 接收所有ICMP报文 */
+    recvPacket(pingResult);
+    pingResult.nsend = m_nsend;
+    pingResult.nreceived = m_nreceived;
+    close(m_sockfd);
+    return true;
 }
--- ComputerNetwork/ping/run.sh
+++ ComputerNetwork/ping/run.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
 docker run -d --name compile gdb:ubuntu
-docker cp sendRawPacket.cpp compile:/send.cpp
-docker cp receiveRawpacket.cpp compile:/receive.cpp
-docker exec compile bash -c "g++ -g -std=c++11 send.cpp -o send"
-docker exec compile bash -c "g++ -g -std=c++11 receive.cpp -o receive"
-docker cp compile:/send send
-docker cp compile:/receive receive
+docker cp ping.h compile:/
+docker cp ping.cpp compile:/
+docker cp main.cpp compile:/
+docker exec compile bash -c "g++ ping.cpp main.cpp -o ping"
+docker cp compile:/ping .
 
 docker network create VMNet --subnet 172.0.0.0/28 --gateway 172.0.0.14
 
-docker run -d --name container1 network:ubuntu
+docker run -d --privileged --name container1 network:ubuntu bash -c "tshark -w container1.tshark"
 docker network connect VMNet container1 --ip 172.0.0.1
 docker network disconnect bridge container1
-docker cp send container1:/send
+docker cp ping container1:/ping
 
 docker run -d --name container2 network:ubuntu
 docker network connect VMNet container2 --ip 172.0.0.2
 docker network disconnect bridge container2
-docker cp receive container2:/receive
+docker cp ping container2:/ping
 
-docker exec container2 bash -c "./receive" &
-docker exec container1 bash -c "./send"
+docker exec container1 bash -c "./ping 172.0.0.2"
+docker exec container2 bash -c "./ping 172.0.0.1"
 
-echo "container1's logs:\n"
-docker logs container1
-echo "container2's logs:\n"
-docker logs container1
-echo "\n"
+docker cp container1:/container1.tshark ping.tshark
 
 docker rm -f compile container1 container2
 docker network rm VMNet
-rm send receive
+rm ping
