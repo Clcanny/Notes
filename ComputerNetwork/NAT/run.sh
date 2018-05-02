f910cb6c278ecb3a00027bf8392c75980bd74a90
demons <837940593@qq.com>
2018-05-02 22:47:17 +0800

    .

--- ComputerNetwork/NAT/run.sh
+++ ComputerNetwork/NAT/run.sh
@@ -1,26 +1,35 @@
 #/bin/bash
-docker network create --subnet 172.0.0.0/20 --ip-range 172.0.0.0/25 --gateway 172.0.0.126 subnet1
-docker network create --subnet 172.0.0.0/20 --ip-range 172.0.1.0/30 --gateway 172.0.1.3 subnet2
+docker network create --subnet 172.0.0.0/24 --gateway 172.0.0.254 insideSubnet
+docker network create --subnet 172.1.0.0/24 --gateway 172.1.0.254 fakeInternet
 
-# docker run -d --privileged --name router network:ubuntu
-# docker network disconnect bridge router
-# docker network connect VMNet router --ip 172.0.0.0 --ip 172.0.0.10
-# docker inspect router
-# docker exec router bash -c "hostname -i"
-# docker exec router bash -c "ip link"
-# 
-# docker run -d --privileged --name pc1 network:ubuntu
-# docker network disconnect bridge pc1
-# docker network connect VMNet pc1 --ip 172.0.0.1
-# 
-# docker run -d --privileged --name pc2 network:ubuntu
-# docker network disconnect bridge pc2
-# docker network connect VMNet pc2 --ip 172.0.0.2
-# 
-# docker run -d --privileged --name pc3 network:ubuntu
-# docker network disconnect bridge pc3
-# docker network connect VMNet pc3 --ip 172.0.0.3
-# 
-# docker rm -f router
-# docker rm -f pc1 pc2 pc3
-docker network rm subnet1 subnet2
+docker run -d --privileged --name natRouter network:ubuntu
+docker network disconnect bridge natRouter
+docker network connect insideSubnet natRouter --ip 172.0.0.253
+docker network connect fakeInternet natRouter --ip 172.1.0.252
+docker exec natRouter bash -c "ip route change default via 172.1.0.253 dev eth2"
+docker exec natRouter bash -c "apt-get install -y iptables"
+docker exec natRouter bash -c "iptables -t nat -A POSTROUTING -s 172.0.0.1/32 -o eth1 -j SNAT --to 172.1.0.1"
+docker exec natRouter bash -c "iptables -t nat -A POSTROUTING -s 172.0.0.2/32 -o eth1 -j SNAT --to 172.1.0.2"
+docker exec natRouter bash -c "iptables -L POSTROUTING -t nat"
+
+docker run -d --privileged --name firstHopRouter network:ubuntu
+docker network disconnect bridge firstHopRouter
+docker network connect fakeInternet firstHopRouter --ip 172.1.0.253
+docker exec firstHopRouter bash -c "ip route add 172.1.0.0/30 via 172.1.0.252 dev eth1"
+
+docker run -d --privileged --name pc1 network:ubuntu
+docker network disconnect bridge pc1
+docker network connect insideSubnet pc1 --ip 172.0.0.1
+docker exec pc1 bash -c "ip route change default via 172.0.0.253 dev eth1"
+
+docker run -d --privileged --name pc2 network:ubuntu
+docker network disconnect bridge pc2
+docker network connect insideSubnet pc2 --ip 172.0.0.2
+docker exec pc2 bash -c "ip route change default via 172.0.0.253 dev eth1"
+
+docker exec pc1 bash -c "ping 172.0.0.253 -c 4"
+docker exec pc1 bash -c "ping 172.1.0.253 -c 4"
+
+# docker rm -f natRouter
+# docker rm -f pc1 pc2
+# docker network rm insideSubnet
---
+++ ComputerNetwork/run.sh
@@ -0,0 +1,82 @@
+#/bin/bash
+docker network create VMNet1 --subnet 192.168.2.0/25 --gateway 192.168.2.126
+docker network create VMNet2 --subnet 192.168.2.128/27 --gateway 192.168.2.158
+docker network create NetworkBetweenRouters --subnet 210.28.120.0/24 --gateway 210.28.120.254
+docker network create Internet --subnet 210.28.130.0/24 --gateway 210.28.130.254
+
+docker run -d --privileged --name router1 network:ubuntu
+docker network disconnect bridge router1
+docker network connect VMNet1 router1 --ip 192.168.2.1
+docker network connect VMNet2 router1 --ip 192.168.2.129
+docker network connect NetworkBetweenRouters router1 --ip 210.28.120.1
+
+docker run -d --privileged --name router2 network:ubuntu
+docker network disconnect bridge router2
+docker network connect NetworkBetweenRouters router2 --ip 210.28.120.2
+docker network connect Internet router2 --ip 210.28.130.1
+
+# 删除默认的路由规则
+docker exec router1 bash -c "ip route delete default"
+docker exec router1 bash -c "ip route delete 192.168.2.0/25"
+docker exec router1 bash -c "ip route delete 192.168.2.128/27"
+docker exec router1 bash -c "ip route delete 210.28.120.0/24"
+# 添加三条路由规则
+docker exec router1 bash -c "ip route add default via 210.28.120.2 dev eth3"
+docker exec router1 bash -c "ip route add 192.168.2.0/25 via 192.168.2.1 dev eth1"
+docker exec router1 bash -c "ip route add 192.168.2.128/27 via 192.168.2.129 dev eth2"
+# 显示新的路由规则
+docker exec router1 bash -c "ip route"
+# docker exec router1 bash -c "iptables -t nat -A POSTROUTING -s 192.168.2.2 -o eth3 -j SNAT --to-source 210.28.130.3"
+
+# 删除默认的路由规则
+docker exec router2 bash -c "ip route delete default"
+docker exec router2 bash -c "ip route delete 210.28.120.0/24"
+docker exec router2 bash -c "ip route delete 210.28.130.0/24"
+# 添加三条路由规则
+docker exec router1 bash -c "ip route add default via 210.28.120.1 dev eth1"
+docker exec router1 bash -c "ip route add 210.28.130.0/24 via 210.28.130.1 dev eth2"
+# 显示新的路由规则
+docker exec router1 bash -c "ip route"
+
+docker run -d --privileged --name container1 network:ubuntu
+docker network disconnect bridge container1
+docker network connect VMNet1 container1 --ip 192.168.2.2
+docker exec container1 bash -c "ip route change default via 192.168.2.1 dev eth1"
+
+docker run -d --privileged --name container2 network:ubuntu
+docker network disconnect bridge container2
+docker network connect VMNet1 container2 --ip 192.168.2.3
+docker exec container2 bash -c "ip route change default via 192.168.2.1 dev eth1"
+
+docker run -d --privileged --name container3 network:ubuntu
+docker network disconnect bridge container3
+docker network connect VMNet2 container3 --ip 192.168.2.130
+docker exec container3 bash -c "ip route change default via 192.168.2.129 dev eth1"
+
+docker run -d --privileged --name container4 network:ubuntu
+docker network disconnect bridge container4
+docker network connect Internet container4 --ip 210.28.130.2
+docker exec container4 bash -c "ip route change default via 210.28.130.1 dev eth1"
+docker exec -d container4 bash -c "tshark -w test.tshark"
+
+# docker exec container1 bash -c "ping 192.168.2.3 -c 4"
+# docker exec container1 bash -c "ping 192.168.2.130 -c 4"
+docker exec container1 bash -c "ping 210.28.130.2 -c 1"
+
+# docker exec container2 bash -c "ping 192.168.2.2 -c 4"
+# docker exec container2 bash -c "ping 192.168.2.130 -c 4"
+# docker exec container2 bash -c "ping 210.28.130.3 -c 4"
+
+# docker exec container4 bash -c "ping 192.168.2.2 -c 4"
+# docker exec container4 bash -c "ping 192.168.2.3 -c 4"
+# docker exec container4 bash -c "ping 192.168.2.130 -c 4"
+
+# docker exec container3 bash -c "ping 192.168.2.2 -c 4"
+# docker exec container3 bash -c "ping 192.168.2.3 -c 4"
+# docker exec container3 bash -c "ping 210.28.130.3 -c 4"
+
+sleep 5
+docker cp container4:/test.tshark .
+
+docker rm -f router1 router2 container1 container2 container4 container3
+docker network rm VMNet1 VMNet2 NetworkBetweenRouters Internet
